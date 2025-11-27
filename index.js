90f28a32b35a15c36d84c2909a1ec87bf15ff150
jermackov <j.ermackov@gmail.com>
2025-11-27 21:58:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -67,25 +67,20 @@ client.on("messageCreate", async (message) => {
   
 //Day 2
 if (message.content.toLowerCase() === "pflanze 70") {
+    const text = `Hallo Jagi, ich hoffe, du hast gut geschlafen :)
 
-  message.reply(
-    `Hallo Jagi, ich hoffe du hast gut geschlafen :)\n\n` +
-    `Nuna hat mir erzählt, dass sie unbedingt mit dir zusammenziehen will und mit dir eine Ente als Haustier holen möchte. ` +
-    `Leider wird das wahrscheinlich noch sehr lange nicht möglich sein. Das zu hören hat mich echt ein bisschen traurig gemacht, ` +
-    `weshalb ich eine süße Alternative für euch habe.\n\n` +
+Nuna hat mir erzählt, dass sie unbedingt mit dir zusammenziehen will und zusammen mit dir eine Ente als Haustier holen will, aber dass das wahrscheinlich noch sehr lange nicht möglich sein wird. Das zu hören hat mich echt ein bisschen traurig gemacht, weshalb ich eine süße Alternative für euch habe.
 
-    `Diese Pflanze ist nicht einfach irgendeine Pflanze. Das ist eine Sukkul*ente* 🥰. ` +
-    `Solange ihr euch keine richtige Ente holen könnt, ist diese Ente solange euer Haustier.\n\n` +
+Diese Pflanze ist nicht einfach irgendeine Pflanze. Das ist eine Sukkul*ente* 🥰. Solange ihr euch keine richtige Ente holen könnt, ist diese Ente solange euer Haustier.
 
-    `Ich habe gehört, dass du dir Sorgen machst, dass du dich nicht gut um Pflanzen kümmern kannst. ` +
-    `Bei Sukkulenten ist das kein Problem! Sie sind sehr pflegeleicht, benötigen wenig Wasser, mögen viel Licht ` +
-    `und wachsen auch in kleineren Töpfen problemlos.\n\n` +
+Ich habe gehört, dass du dir Sorgen machst, dass du dich nicht gut um Pflanzen kümmern kannst. Bei Sukkulenten ist das kein Problem! Sie sind sehr pflegeleichte Pflanzen, da sie nur wenig Wasser benötigen, viel Licht bevorzugen und auch in kleineren Töpfen problemlos wachsen.
 
-    `Wenn sie sich wohlfühlen, zeigen sie es dir im Frühling oder Sommer mit hübschen Blüten an langen Stielen – ` +
-    `ein kleines Dankeschön dafür, dass du sie so entspannt und liebevoll behandelst ♥️.\n\n` +
+Wenn sie sich wohlfühlen, zeigen sie es dir im Frühling oder Sommer mit hübschen Blüten, die an langen Stielen erscheinen – ein kleines Dankeschön dafür, dass du sie so entspannt und liebevoll behandelst ♥️ Wenn du sehr nett zu ihr bist, lernt sie ja vielleicht auch zu quaken.`;
 
-    `Und wer weiß… wenn du sehr nett zu ihr bist, lernt sie vielleicht sogar zu quaken 😉.`
-  );
+ await message.reply(text);
+    return; // damit danach nichts anderes mehr ausgeführt wird
+  }
+});
 
   return; // verhindert, dass weiterer Code ausgeführt wird
 }
