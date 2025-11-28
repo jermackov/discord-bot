05040772bbe2e4152693869d607ba1c5867aed73
jermackov <j.ermackov@gmail.com>
2025-11-28 13:45:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -150,7 +150,7 @@ client.on("messageCreate", async (message) => {
 
   const content = message.content.toLowerCase();
 
- if (content === "minecraft egzw") {
+ if (content === "minecraft egzv") {
     const text = `selam aleykum Jagi, ich hoffe du hast heute schön gebetet.
 
 Gute Arbeit bei der letzten Aufgabe, es ist sehr beeindruckend, dass du weißt wie Gras in welchem Land aussieht!! Als Belohnung möchte ich dir diese Kekse schenken. Vielleicht kannst du ja ein paar Enten damit füttern :)`;
@@ -165,7 +165,7 @@ Gute Arbeit bei der letzten Aufgabe, es ist sehr beeindruckend, dass du weißt w
       .setDescription(
         "Du bist gefangen in einem Raum. Dein Ziel ist es herauszukommen " +
         "und herauszufinden, in welcher Stadt du dich befindest.\n\n" +
-        "IP: **jagi.aternos.me**"
+        "**jagi.aternos.me**"
       );
 
     // Embed senden
